fca358aebd7161e8fd8f5a5dff9560cb93870fdf
unknown <lgal.martin@gmail.com>
2022-11-11 19:44:12 +0100

    primera version

---
+++ app/src/main/java/com/example/a2do/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.a2do;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/a2do/NuevaCuenta.java
@@ -0,0 +1,4 @@
+package com.example.a2do;
+
+public class NuevaCuenta {
+}
